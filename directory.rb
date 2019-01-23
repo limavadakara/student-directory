88aa82dc0f8d132fc5976eea5b118d83bba1f704
Lima Vadakara <lima.vadakara@gmail.com>
2019-01-23 16:02:05 +0000

    Print students that start with a given input starting alphabet. No change to the way the numbers are printed

--- directory.rb
+++ directory.rb
@@ -3,9 +3,9 @@ def print_header
   puts  "-------------"
 end
 
-def print(students)
+def print(students, name_starts_with)
   students.each_with_index do |student, idx|
-    puts "#{idx+1} #{student[:name]} (#{student[:cohort]}) cohort" if student[:name].capitalize.start_with? "A" 
+    puts "#{idx+1} #{student[:name]} (#{student[:cohort]}) cohort" if student[:name].capitalize.start_with? name_starts_with 
   end
 end
 
@@ -28,7 +28,14 @@ def input_students
   return students
 end
 
+def obtain_name_start
+    puts "Enter the starting letter of the names you want to print"
+    first_alpha = gets.chomp.slice(0,1).upcase
+    return first_alpha
+end
+
 students = input_students  
+start_alphabet = obtain_name_start
 print_header
-print(students)
+print(students, start_alphabet)
 print_footer(students)
