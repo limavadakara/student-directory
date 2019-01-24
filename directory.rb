0dda34a643b3a032e8f61c24e0ac1115f1a01a27
Lima Vadakara <lima.vadakara@gmail.com>
2019-01-24 17:08:51 +0000

    split the interactive menu method - refactor

--- directory.rb
+++ directory.rb
@@ -1,23 +1,30 @@
+@students = []
 def interactive_menu
-  students = []
   loop do
-    puts "What would you like to do?"
-    puts "1. Input students"
-    puts "2. Print students"
-    puts "9. Exit"
-    choice = gets.chomp
-    case choice
-      when "1" 
-        input_students(students)
-      when "2"
-        print_header
-        print(students, obtain_name_start())
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "Invalid selection, please try again"
-    end
+    print_menu
+    process_choice(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "What would you like to do?"
+  puts "1. Input students"
+  puts "2. Print students"
+  puts "9. Exit"
+end 
+
+def process_choice(choice)
+  case choice
+    when "1"
+      input_students()
+    when "2"
+      print_header
+      print(obtain_name_start())
+      print_footer()
+    when "9"
+      exit
+    else
+      puts "Invalid selection, please try again"
   end
 end
 
@@ -26,26 +33,26 @@ def print_header
   puts  "-------------"
 end
 
-def print(students, name_starts_with)
+def print(name_starts_with)
   i = 0
-#  while i < students.length do
-  until i == students.length do
-    if (students[i][:name].capitalize.start_with? name_starts_with) || (students[i][:name].length < 12)
-      puts "#{i + 1} #{students[i][:name]} (#{students[i][:cohort]}) cohort with #{students[i][:hobbies]} hobbies from #{students[i][:country]} and with height of #{students[i][:height]}".center(100 ,"-")
+#  while i < @students.length do
+  until i == @students.length do
+    if (@students[i][:name].capitalize.start_with? name_starts_with) || (@students[i][:name].length < 12)
+      puts "#{i + 1} #{@students[i][:name]} (#{@students[i][:cohort]}) cohort with #{@students[i][:hobbies]} hobbies from #{@students[i][:country]} and with height of #{@students[i][:height]}".center(100 ,"-")
     end
     i += 1
   end 
-#  students.each_with_index do |student, idx|
+#  @students.each_with_index do |student, idx|
 #     puts "#{idx+1} #{student[:name]} (#{student[:cohort]}) cohort" if student[:name].capitalize.start_with? name_starts_with 
 #     puts "#{idx+1} #{student[:name]} (#{student[:cohort]}) cohort" if student[:name].length < 12 
 #  end
 end
 
-def print_footer(students)
-  puts students.count == 1 ? "Overall, we have #{students.count()} great student" : "Overall, we have #{students.count()} great students"
+def print_footer()
+  puts @students.count == 1 ? "Overall, we have #{@students.count()} great student" : "Overall, we have #{@students.count()} great students"
 end
 
-def input_students(students)
+def input_students()
   puts "Enter student details"
   puts "Press enter twice to finish"
   while true do
@@ -74,11 +81,11 @@ def input_students(students)
     if height.empty?
       height = "not known height"
     end
-    students.push({name: name, cohort: cohort, hobbies: hobbies, country: country, height: height})
+    @students.push({name: name, cohort: cohort, hobbies: hobbies, country: country, height: height})
 
-    puts students.count == 1? "Now we have #{students.count} student" : "Now we have #{students.count} students"
+    puts @students.count == 1? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
   end
-#  return students
+  return @students
 end
 
 def obtain_name_start
@@ -87,9 +94,9 @@ def obtain_name_start
     return first_alpha
 end
 
-def print_students_grouped_by_cohort(students)
+def print_students_grouped_by_cohort()
   cohorts_grouping = Hash.new
-  students.map { |student|
+  @students.map { |student|
     cohort = student[:cohort]
     name = student[:name]
     if cohorts_grouping[cohort.to_sym] == nil
