94959afe1d9c807fbbbf2b07bb1d0d1f6a6221ce
Lima Vadakara <lima.vadakara@gmail.com>
2019-01-25 11:37:14 +0000

    clean up code, add option 5 on the menu

--- directory.rb
+++ directory.rb
@@ -1,4 +1,6 @@
 @students = []
+
+
 def interactive_menu
   loop do
     print_menu
@@ -6,14 +8,17 @@ def interactive_menu
   end
 end
 
+
 def print_menu
   puts "What would you like to do?"
   puts "1. Input students"
   puts "2. Print students"
   puts "3. Save student list to students.csv"
   puts "4. Load student list from students.csv"
+  puts "5. Print students starting with a particular Letter" 
   puts "9. Exit"
 end 
+
 
 def process_choice(choice)
   case choice
@@ -25,6 +30,8 @@ def process_choice(choice)
       save_students()
     when "4"
       load_students()
+    when "5"
+      print_students_name_starting_with()
     when "9"
       exit
     else
@@ -32,35 +39,48 @@ def process_choice(choice)
   end
 end
 
+
 def show_students
   print_header
-  print_students_list(obtain_name_start())
+  print_students_list()
   print_footer()
 end
+
 
 def print_header
   puts "The students of Villains Academy"
   puts  "-------------"
 end
 
-def print_students_list(name_starts_with)
+
+def print_students_list()
   i = 0
-#  while i < @students.length do
   until i == @students.length do
-    if (@students[i][:name].capitalize.start_with? name_starts_with) || (@students[i][:name].length < 12)
+    if (@students[i][:name].length < 12)
       puts "#{i + 1} #{@students[i][:name]} (#{@students[i][:cohort]}) cohort with #{@students[i][:hobbies]} hobbies from #{@students[i][:country]} and with height of #{@students[i][:height]}".center(100 ,"-")
     end
     i += 1
   end 
-#  @students.each_with_index do |student, idx|
-#     puts "#{idx+1} #{student[:name]} (#{student[:cohort]}) cohort" if student[:name].capitalize.start_with? name_starts_with 
-#     puts "#{idx+1} #{student[:name]} (#{student[:cohort]}) cohort" if student[:name].length < 12 
-#  end
 end
+
+
+def print_students_name_starting_with()
+  name_starts_with = obtain_name_start()
+  count = 0  
+  @students.each_with_index do |student, idx|
+     if student[:name].capitalize.start_with? name_starts_with
+       puts "#{idx + 1} #{student[:name]} (#{student[:cohort]}) cohort with #{student[:hobbies]} hobbies from #{student[:country]} and with height of #{student[:height]}".center(100 ,"-")
+       count += 1
+     end
+  end
+  puts "No students who have name starting with #{name_starts_with}" if count == 0
+end
+
 
 def print_footer()
   puts @students.count == 1 ? "Overall, we have #{@students.count()} great student" : "Overall, we have #{@students.count()} great students"
 end
+
 
 def input_students()
   puts "Enter student details"
@@ -98,11 +118,13 @@ def input_students()
   return @students
 end
 
+
 def obtain_name_start
-    puts "Enter the starting letter of the names you want to print"
-    first_alpha = STDIN.gets.chomp.slice(0,1).upcase
-    return first_alpha
+  puts "Enter the starting letter of the names you want to print"
+  first_alpha = STDIN.gets.chomp.slice(0,1).upcase
+  return first_alpha
 end
+
 
 def print_students_grouped_by_cohort()
   cohorts_grouping = Hash.new
@@ -119,6 +141,8 @@ def print_students_grouped_by_cohort()
         puts "Cohort #{key} has #{value.join(", ")}"
   end
 end
+
+
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
@@ -127,6 +151,8 @@ def save_students
   end
   file.close()
 end
+
+
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
@@ -135,6 +161,7 @@ def load_students(filename = "students.csv")
   end
   file.close()  
 end
+
 
 def try_file_load
   filename = ARGV.first
@@ -148,5 +175,7 @@ def try_file_load
     puts "#{filename} does not exist so couldn't be loaded"
   end
 end
+
+
 try_file_load
 interactive_menu
