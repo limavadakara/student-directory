2a0bab9936894eaf659312fd7f406db0a9534a8c
Lima Vadakara <lima.vadakara@gmail.com>
2019-01-27 20:54:03 +0000

    Add __FILE__ to indicate filename of program being executed

--- read_print_self_data.rb
+++ read_print_self_data.rb
@@ -1,8 +1,8 @@
-filename = $0
+filename = __FILE__
 File.open(filename, "r") do |file|
   file.readlines.each do |line|
     puts line
   end
 end
-# using filename = $PROGRAM_NAME will also give the same result
+# using filename = $PROGRAM_NAME or $0 will also give the same result
 
